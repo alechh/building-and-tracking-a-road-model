d3d088144089bf4f5248c22f43e44c264922efca
Aleksandr <alex.sergeevich1348@gmail.com>
2021-11-17 16:31:12 +0300

    unit test for curvature calculation using googletest framework

--- main.cpp
+++ main.cpp
@@ -244,87 +244,6 @@ void test_calculation_of_curvature(const std::string& PATH)
 }
 
 
-std::vector<Point> get_circle_contour(const int& R)
-{
-    std::vector<Point> contour;
-    double t = 0;
-    double epsilon = 10e-4;
-    while (t < 2 * CV_PI)
-    {
-        contour.emplace_back(500 + R * cos(t),300 + R * sin(t));
-        t += epsilon;
-        //std::cout << "t = " << t << std::endl;
-    }
-    return contour;
-}
-
-
-void test_circle_curvature()
-{
-    // Curvature of circle = 1/R, where R -- radius of the circle
-
-    const int radius = 10000000;
-
-    Mat image_contours(600, 1000, 16);
-
-    std::vector<double> circle_curvature;
-
-    std::vector< std::vector<Point> > contours;
-    std::vector<Point> circle_contour = get_circle_contour(radius);
-    contours.emplace_back(circle_contour);
-    std::cout << "Circle contour size = " << circle_contour.size() << std::endl;
-
-//        for (const auto& i : circle_contour)
-//        {
-//            std::cout << i << std::endl;
-//        }
-
-    drawContours(image_contours, contours, 0, Scalar(255, 255, 255));
-    circle_curvature = calculate_curvature(circle_contour, 1);
-
-    double max_error = 0, min_error = 1;
-    int count_inf = 0, count_zero = 0;
-
-    for (auto i : circle_curvature)
-    {
-        if (i == circle_curvature[0])
-        {
-            continue;
-        }
-
-        //std::cout << "i = " << i << std::endl;
-
-        if (i == std::numeric_limits<double>::infinity())
-        {
-            count_inf++;
-            continue;
-        }
-        if (i == 0)
-        {
-            count_zero++;
-            continue;
-        }
-
-        double error = std::abs(1.0 / radius - i);
-
-        if (error > max_error)
-        {
-            max_error = error;
-        }
-        if (error < min_error)
-        {
-            min_error = error;
-        }
-        //std::cout << "Error = " << error << std::endl;
-    }
-    std::cout <<  "Max Error = " << max_error << std::endl;
-    std::cout << "Min Error = " << min_error << std::endl;
-    std::cout << std::endl << "Number of inf = " << count_inf << std::endl;
-    std::cout << "Number of zero = " << count_zero << std::endl;
-    std::cout << "Number of non inf/zero = " << circle_contour.size() - count_zero - count_inf << std::endl;
-}
-
-
 int main()
 {
     const std::string PATH1 = "../videos/video.mp4";
@@ -340,7 +259,6 @@ int main()
     //find_lines(PATH2);
     //play_video(PATH1);
     //calculation_of_curvature(PATH2);
-    test_circle_curvature();
 
     return 0;
 }
