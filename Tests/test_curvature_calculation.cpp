5be83038085a4df538123f83f017d850fde90839
Aleksandr <alex.sergeevich1348@gmail.com>
2021-11-18 19:54:39 +0300

    unit tests for calculating curvature of vertical and horizontal straight lines and parabola

--- Tests/test_curvature_calculation.cpp
+++ Tests/test_curvature_calculation.cpp
@@ -13,13 +13,45 @@ std::vector<cv::Point> get_circle_contour(const int& R)
     {
         contour.emplace_back(500 + R * cos(t),300 + R * sin(t));
         t += epsilon;
-        //std::cout << "t = " << t << std::endl;
     }
     return contour;
 }
 
 
-TEST(BasicTest, CurvatureCalculation)
+std::vector<cv::Point> get_straight_line_contour(bool isVertical = true)
+{
+    std::vector<cv::Point> contour;
+    int t = 0;
+    while(t < 100)
+    {
+        if (isVertical)
+        {
+            contour.emplace_back(cv::Point(0, t));
+        }
+        else
+        {
+            contour.emplace_back(cv::Point(t, 0));
+        }
+        t++;
+    }
+    return contour;
+}
+
+
+std::vector<cv::Point> get_parabola_contour(int step, int left_boundary, int right_boundary)
+{
+    std::vector<cv::Point> contour;
+    int x = left_boundary;
+    while (x < right_boundary)
+    {
+        contour.emplace_back(cv::Point(x,3 * x * x));
+        x += step;
+    }
+    return contour;
+}
+
+
+TEST(CurvatureTest, CircleCurvature)
 {
     // Curvature of circle = 1/R, where R -- radius of the circle
 
@@ -32,7 +64,6 @@ TEST(BasicTest, CurvatureCalculation)
     std::vector< std::vector<cv::Point> > contours;
     std::vector<cv::Point> circle_contour = get_circle_contour(radius);
     contours.emplace_back(circle_contour);
-    //std::cout << "Circle contour size = " << circle_contour.size() << std::endl;
 
     circle_curvature = ut.calculate_curvature(circle_contour, 1);
 
@@ -45,8 +76,6 @@ TEST(BasicTest, CurvatureCalculation)
         {
             continue;
         }
-
-        //std::cout << "i = " << i << std::endl;
 
         if (i == std::numeric_limits<double>::infinity())
         {
@@ -69,17 +98,135 @@ TEST(BasicTest, CurvatureCalculation)
         {
             min_error = error;
         }
-        //std::cout << "Error = " << error << std::endl;
     }
-    //std::cout <<  "Max Error = " << max_error << std::endl;
-    //std::cout << "Min Error = " << min_error << std::endl;
-    //std::cout << std::endl << "Number of inf = " << count_inf << std::endl;
-    //std::cout << "Number of zero = " << count_zero << std::endl;
-    //std::cout << "Number of non inf/zero = " << circle_contour.size() - count_zero - count_inf << std::endl;
 
     EXPECT_EQ(count_zero, 0);
     EXPECT_EQ(count_inf, 0);
-    ASSERT_LE(max_error, 2.19706e-08);
-    ASSERT_LE(min_error, 3.6e-14);
+    EXPECT_LE(max_error, 2.19706e-08);
+    EXPECT_LE(min_error, 3.6e-14);
 }
 
+
+TEST(CurvatureTest, StraightVerticalLineCurvature)
+{
+    // Curvature of the straight line = 0
+
+    Utils ut;
+    std::vector<double> lineCurvature;
+
+    std::vector< std::vector<cv::Point> > contours;
+    std::vector<cv::Point> line_contour = get_straight_line_contour();
+    contours.emplace_back(line_contour);
+
+    lineCurvature = ut.calculate_curvature(line_contour, 1);
+
+    double max_error = 0, min_error = 1;
+
+    for (auto i : lineCurvature)
+    {
+        if (i == lineCurvature[0] || i == lineCurvature[lineCurvature.size() - 1])
+        {
+            continue;
+        }
+
+        double error = std::abs(i);
+
+        if (error > max_error)
+        {
+            max_error = error;
+        }
+        if (error < min_error)
+        {
+            min_error = error;
+        }
+    }
+
+    EXPECT_EQ(max_error, 0);
+    EXPECT_EQ(min_error, 0);
+}
+
+
+TEST(CurvatureTest, StraightHorizontalLineCurvature)
+{
+    // Curvature of the straight line = 0
+
+    Utils ut;
+    std::vector<double> lineCurvature;
+
+    std::vector< std::vector<cv::Point> > contours;
+    std::vector<cv::Point> line_contour = get_straight_line_contour(false);
+    contours.emplace_back(line_contour);
+
+    lineCurvature = ut.calculate_curvature(line_contour, 1);
+
+    double max_error = 0, min_error = 1;
+
+    for (auto i : lineCurvature)
+    {
+        if (i == lineCurvature[0] || i == lineCurvature[lineCurvature.size() - 1])
+        {
+            continue;
+        }
+
+        double error = std::abs(i);
+
+        if (error > max_error)
+        {
+            max_error = error;
+        }
+        if (error < min_error)
+        {
+            min_error = error;
+        }
+    }
+
+    EXPECT_EQ(max_error, 0);
+    EXPECT_EQ(min_error, 0);
+}
+
+
+TEST(CurvatureTest, ParabolaCurvature)
+{
+    // Curvature of parabola 3x^2 = 6 / (1 + 36 * x * x)^(3 / 2)
+
+    Utils ut;
+    std::vector<double> parabolaCurvature;
+
+    int left_x_boundary = -50;
+    int right_x_boundary = 50;
+    int x_step = 1;
+
+    std::vector< std::vector<cv::Point> > contours;
+    std::vector<cv::Point> parabola_contour = get_parabola_contour(x_step, left_x_boundary, right_x_boundary);
+    contours.emplace_back(parabola_contour);
+
+    parabolaCurvature = ut.calculate_curvature(parabola_contour, 1);
+
+    double x = left_x_boundary;
+    int i = 0;
+    int count_inf = 0;
+    while (x < right_x_boundary)
+    {
+        if (i == 0 || i == parabola_contour.size() - 1)
+        {
+            x += x_step;
+            i++;
+            continue;
+        }
+        if (parabolaCurvature[i] == std::numeric_limits<double>::infinity())
+        {
+            i++;
+            x += x_step;
+            count_inf++;
+            continue;
+        }
+
+        double exactValue = 6.0 / pow(1 + 36 * pow(x ,2), 1.5);
+        EXPECT_EQ(parabolaCurvature[i],  exactValue);
+
+        x += x_step;
+        i++;
+    }
+
+    EXPECT_EQ(count_inf, 0);
+}
