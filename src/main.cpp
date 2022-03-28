0f926ee8ca17dce756fbfd39a78ff5163f822153
Aleksandr <alex.sergeevich1348@gmail.com>
2022-03-28 17:40:18 +0300

    a very simple separation into the left and right side of the lane

--- src/main.cpp
+++ src/main.cpp
@@ -28,11 +28,10 @@ void saveContoursOnImage(const std::vector<std::vector<cv::Point>> &contours)
 
 
 
-void calculateMeanError(double &minCurvatureError, int &minStepCurvature, const int CUR_STEP, const std::vector<double> &contourCurvature)
+void calculateMeanError(double &minCurvatureError, int &minStepCurvature, const double EXACT_CURVATURE, const int CUR_STEP, const std::vector<double> &contourCurvature)
 {
     double meanCurvature = 0;
     int count = 0;
-    const double EXACT_CURVATURE = 0.01;
     for (auto i: contourCurvature)
     {
         if (i != 0)
@@ -57,9 +56,8 @@ void buildRoadModelByContour()
     const int COLS = 1000;
     const int TYPE = 16;
 
-    std::vector< std::vector<cv::Point> > contours;
-
-    contours.emplace_back(ContourBuilder::getSimpleRightContour2());
+    std::vector< std::vector<cv::Point> > contours = ContourBuilder::getRightAndLeftContours();
+    const double EXACT_CURVATURE = 0.01;
 
     saveContoursOnImage(contours);
 
@@ -70,7 +68,7 @@ void buildRoadModelByContour()
     double minCurvatureError = 10000;
     int minStepCurvature = 0;
 
-    for (int iStep = 190; iStep <= 190; ++iStep)
+    for (int iStep = 10; iStep <= 10; ++iStep)
     {
         //std::cout << "iStep = " << iStep << std::endl << contours[0].size() << std::endl;
 
@@ -88,19 +86,30 @@ void buildRoadModelByContour()
 
             contoursCurvatures[i] = std::move(tempCurvatureContour);
 
-            ExperimentWithCurvatureCalculation::buildRoadModelBasedOnTheSingleContour(roadModel, contours[i], contoursCurvatures[i]);
+            bool isRightContour;
+            if (contours[i][0].x < COLS / 2)
+            {
+                isRightContour = false;
+            }
+            else
+            {
+                isRightContour = true;
+            }
+
+            ExperimentWithCurvatureCalculation::buildRoadModelBasedOnTheSingleContour(roadModel, contours[i],
+                                                                                      contoursCurvatures[i], isRightContour);
 
             ExperimentWithCurvatureCalculation::showCurvatureOnImage(curvatureOnContourPicture, contours[i], contoursCurvatures[i]);
 
-            calculateMeanError(minCurvatureError, minStepCurvature, iStep, contoursCurvatures[i]);
+            calculateMeanError(minCurvatureError, minStepCurvature, EXACT_CURVATURE, iStep, contoursCurvatures[i]);
         }
 
         roadModel.drawModel(roadModelPicture);
         imwrite("../images/frameRoadModel.jpg", roadModelPicture);
 
-        //roadModel.printInformationOfTheRightSide();
+        roadModel.printInformationOfTheModel();
 
-        imwrite("../images/curvatureOnContourPicture/curvatureContour.jpg", curvatureOnContourPicture);
+        imwrite(cv::format("../images/curvatureOnContourPicture/curvatureContour%d.jpg", iStep), curvatureOnContourPicture);
 
         roadModel.drawModelPoints(contourPointsPicture);
         imwrite("../images/showModelPoints/modelPoints.jpg", contourPointsPicture);
